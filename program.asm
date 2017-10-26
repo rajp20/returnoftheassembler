5e8121ddd9153f148575685f2b13bc5309146cab
brandonenid <brandonenid@gmail.com>
2017-10-26 10:46:19 -0600

    example asm update

--- program.asm
+++ program.asm
@@ -1,24 +1,24 @@
-Start: 
-	setlo A, 0xff   
-	sethi A, 0xff   
-	setlo B, 0x00   
-	sethi B, 0x00   
-	setlo C, 0x00   
-	sethi C, 0xE0   
-Loop: 
-	loadmem D, B     
-	storemem D, A   	
-	setlo D, 0x01   	
-	sethi D, 0x00   		
-	add A, D        	
-	add B, D        	
-	add C, D
-Break:   	
-	bim Loop      
-	setlo B, 0xff	
-	sethi B, 0xff	
-	loadmem C, B
+Start:
+setlo A, 0xff   
+sethi A, 0xff   
+setlo B, 0x00   
+sethi B, 0x00   
+setlo C, 0x00   
+sethi C, 0xE0   
+Loop:
+loadmem D, B     
+storemem D, A   	
+setlo D, 0x01   	
+sethi D, 0x00 
+add A, D        	
+add B, D
+add C, D
+Break:
+bim Loop      
+setlo B, 0xff	
+sethi B, 0xff	
+loadmem C, B
 Loop2:
-	storemem C, A  
-	add B, D  
-	biz	Start
+storemem C, A  
+add B, D  
+biz	Start
