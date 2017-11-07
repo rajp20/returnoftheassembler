80a07ac5d3989ad5789b685aa154ff621218180a
Raj Patel <rajpatel0820@gmail.com>
2017-11-07 09:37:34 -0700

    added text_glyph_coe file

--- program.asm
+++ program.asm
@@ -1,7 +1,18 @@
 Start:
     loadi 0, $r1
-    loadi -1, $r2
+    loadi 25, $r2
+    loadi 4, $r5
+    shrli 3, $r5
+    addi 1, $r5
+    loadi 0, $lr0
 Loop:
+    cmp $r1, $r2
+    store $r5, $lr0
+    addi 1, $r5
     addi 1, $r1
-    cmp $lr3, $r2
     jumpne Loop
+    jump ResetLoop
+ResetLoop:
+    loadi 0, $r1
+    addi -25, $r5
+    jump Loop
