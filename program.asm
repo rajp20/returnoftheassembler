fe35e8be9fdc163c62118a4fec47342329528fbc
Raj Patel <rajpatel0820@gmail.com>
2017-11-09 13:45:51 -0700

    updated loop

--- program.asm
+++ program.asm
@@ -5,7 +5,13 @@ Start:
     shlli 4, $r5
     addi 1, $r5
     loadi 0, $lr0
+    loadi 0, $r6
+    loadi 8, $r7
+    shlli 5, $r7	
 Loop:
+    addi 1, $r6
+    cpm $r6, $r7
+    jumpne Loop
     store $r5, $lr0
     addi 1, $r5
     addi 1, $r1
@@ -14,5 +20,6 @@ Loop:
     jump ResetLoop
 ResetLoop:
     loadi 0, $r1
+    loadi 0, $r6
     addi -25, $r5
     jump Loop
