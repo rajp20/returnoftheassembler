2cbd3b52b3c4455e333f1c53510545632323ebfa
Raj Patel <rajpatel0820@gmail.com>
2017-11-09 18:21:05 -0700

    fixed assembly

--- program.asm
+++ program.asm
@@ -1,25 +1,24 @@
 Start:
-    loadi 0, $r1
-    loadi 25, $r2
-    loadi 4, $r5
-    shlli 4, $r5
-    addi 1, $r5
-    loadi 0, $lr0
-    loadi 0, $r6
-    loadi 7, $r7
-    shlli 13, $r7	
+	loadi 25, $r2
+	loadi 1, $lr1
+	shlli 13, $lr1
+ResetPC:
+	loadi 0, $lr0
+	loadi 4, $r5
+	shlli 4, $r5
+	addi 1, $r5
+	loadi 0, $r1
 Loop:
-    addi 1, $r6
-    cmp $r6, $r7
-    jumpne Loop
-    store $r5, $lr0
-    addi 1, $r5
-    addi 1, $r1
-    cmp $r1, $r2
-    jumpne Loop
-    jump ResetLoop
+	cmp $lr0, $lr1
+	jumpe ResetPC
+	store $r5, $lr0
+	addi 1, $lr0
+	addi 1, $r5
+	addi 1, $r1
+	cmp $r1, $r2
+	jumpne Loop
+	jump ResetLoop
 ResetLoop:
-    loadi 0, $r1
-    loadi 0, $r6
-    addi -25, $r5
-    jump Loop
+	loadi 0, $r1
+	addi -25, $r5
+	jump Loop
