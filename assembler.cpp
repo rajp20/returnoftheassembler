80c17d7c465e96dd9952e38c189236cb7800a2f9
Raj Patel <rajpatel0820@gmail.com>
2017-12-07 01:27:30 -0700

    reg update, template for color pallets

--- assembler.cpp
+++ assembler.cpp
@@ -14,6 +14,7 @@ bool isLabel(string line);
 void secondPass(string input_file, map<string, int> labelMap);
 void addCodeToCOE(string linesToAdd, string out_file);
 string getBinaryRegDecLabel(string arg, map<string, int> labelMap, int currentIndex);
+void createColorPallet();
 
 map<string, string> opCodeMapToBinary;
 map<string, string> regMapToBinary;
@@ -81,7 +82,8 @@ int main(int argv, char** argc) {
   regMapToBinary["$r19"] = "10010";
   regMapToBinary["$r20"] = "10011";
   regMapToBinary["$r21"] = "10100";
-  regMapToBinary["$r22"] = "10101";
+  
+  regMapToBinary["$read_data"] = "10101";
   
   regMapToBinary["$x"] = "10110";
   regMapToBinary["$y"] = "10111";
@@ -95,11 +97,244 @@ int main(int argv, char** argc) {
   regMapToBinary["$lr2"] = "11110";
   regMapToBinary["$lr3"] = "11111";
 
+  createColorPallet();
+ 
   map<string, int> labelMap;
   labelMap["start"] = 6;
   firstPass(input_file, labelMap);
   secondPass(input_file, labelMap);
   return 0;
+}
+
+void createColorPallet() {
+  ofstream newTextFile("text_glyph_code.coe", ios_base::app);
+
+  ofstream newFile;
+  newFile.open ("text_glyph_code.coe");
+  // Make sure the file is readable.
+  newFile << "memory_initialization_radix=2;\nmemory_initialization_vector=\n\n";
+  int colorCount = 0;
+  for (int row = 0; row < 240; row++) {
+	int modded_row = row % 20;
+	if (modded_row < 2 || (modded_row >= 18 && modded_row < 20)) {
+	  for (int col = 0; col < 80; col++) {
+		if (col == 5 && modded_row == 19) {
+		  colorCount++;
+		}
+		if (col < 5 && colorCount == 0) {
+		  newFile << "0010001000100010,\n";
+		}
+		else if (col < 5) {
+		  newFile << "0001000100010001,\n";
+		}
+		else {
+		  newFile << "0000000000000000,\n";
+		}
+	  }  
+	}
+	else if (modded_row >= 2 && modded_row < 18) {
+	  for (int col = 0; col < 80; col++) {
+		if (colorCount == 0) {
+		  if (col == 0) {
+			newFile << "0010001000000000,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0000000000000000,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0000000000100010,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 1) { // 3
+		  if (col == 0) {
+			newFile << "0001000100110011,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0011001100110011,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0011001100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 2) { // 4
+		  if (col == 0) {
+			newFile << "0001000101000100,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0100010001000100,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0100010000010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 3) { // 5
+		  if (col == 0) {
+			newFile << "0001000101010101,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0101010101010101,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0101010100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 4) { // 6
+		  if (col == 0) {
+			newFile << "0001000101100110,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0110011001100110,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0110011000010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 5) { // 7
+		  if (col == 0) {
+			newFile << "0001000101110111,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "0111011101110111,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "0111011100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 6) { // 8
+		  if (col == 0) {
+			newFile << "0001000110001000,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1000100010001000,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1000100000010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 7) { // 9
+		  if (col == 0) {
+			newFile << "0001000110011001,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1001100110011001,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1001100100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 8) { // 10
+		  if (col == 0) {
+			newFile << "0001000110101010,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1010101010101010,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1010101000010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 9) { // 11
+		  if (col == 0) {
+			newFile << "0001000110111011,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1011101110111011,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1011101100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 10) { // 12
+		  if (col == 0) {
+			newFile << "0001000111001100,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1100110011001100,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1100110000010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+		else if (colorCount == 11) { // 13
+		  if (col == 0) {
+			newFile << "0001000111011101,\n";
+		  }
+		  else if (col > 0 && col < 4) {
+			newFile << "1101110111011101,\n";
+		  }
+		  else if (col == 4) {
+			newFile << "1101110100010001,\n";
+		  }
+		  else {
+			newFile << "0000000000000000,\n";
+		  }
+		}
+	  }	  
+	}
+	else {
+	  for (int col = 0; col < 80; col++) {
+		if (col < 5) {
+		  newFile << "0001000100010001,\n";
+		}
+		else {
+		  newFile << "0000000000000000,\n";
+		}
+	  }  
+	}
+  }
+
+  newFile << "0000000011111111,\n"; // Canvas Color
+  newFile << "0000000010110101,\n"; // Default color border
+  newFile << "0000000010101010,\n"; // Current Slected color border
+  newFile << "0000000011111111,\n"; // 3
+  newFile << "0000000000000000,\n"; // 4
+  newFile << "0000000011000000,\n"; // 5
+  newFile << "0000000010010010,\n"; // 6
+  newFile << "0000000011010101,\n"; // 7
+  newFile << "0000000011000111,\n"; // 8
+  newFile << "0000000000011011,\n"; // 9
+  newFile << "0000000001001101,\n"; // 10
+  newFile << "0000000001011110,\n"; // 11
+  newFile << "0000000000111111,\n"; // 12
+  newFile << "0000000011111011,\n"; // 13
+  newFile << "0000000000111000,\n";
+  newFile << "0000000010100011,\n"; // Cursor color
+						  
+  newFile.close();
+
 }
 
 /*
--- program.asm
+++ program.asm
@@ -2,23 +2,4 @@ Start:
 	loadi 25, $r2
 	loadi 1, $lr1
 	shlli 13, $lr1
-ResetPC:
-	loadi 0, $lr0
-	loadi 4, $r5
-	shlli 4, $r5
-	addi 1, $r5
-	loadi 0, $r1
-Loop:
-	cmp $lr0, $lr1
-	jumpe ResetPC
-	store $r5, $lr0
-	addi 1, $lr0
-	addi 1, $r5
-	addi 1, $r1
-	cmp $r1, $r2
-	jumpne Loop
-	jump ResetLoop
-ResetLoop:
-	loadi 0, $r1
-	addi -25, $r5
-	jump Loop
+	jump Start
